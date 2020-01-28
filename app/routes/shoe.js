8ceccf2989758edaca9b1cc6f9c6209e6f6f70b1
mozoria <m.ozoria@yahoo.com>
2020-01-28 08:30:20 -0500

    Added routes for shoes
    Added the routes for CRUD

--- app/routes/shoe.js
+++ app/routes/shoe.js
@@ -9,37 +9,37 @@ const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
 // INDEX
-// GET /questions
+// GET /shoes
 router.get('/shoes', requireToken, (req, res, next) => {
   Shoe.find()
     .populate('owner', 'email')
     .then(shoes => {
-      // `questions` will be an array of Mongoose documents
+      // `shoes` will be an array of Mongoose documents
       // we want to convert each one to a POJO, so we use `.map` to
       // apply `.toObject` to each one
       return shoes.map(shoe => shoe.toObject())
     })
-    // respond with status 200 and JSON of the questions
+    // respond with status 200 and JSON of the shoes
     .then(shoes => res.status(200).json({ shoes: shoes }))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
 
 // SHOW
-// GET /questions/5a7db6c74d55bc51bdf39793
-router.get('/questions/:id', requireToken, (req, res, next) => {
+// GET /shoes/5a7db6c74d55bc51bdf39793
+router.get('/shoes/:id', requireToken, (req, res, next) => {
   // req.params.id will be set based on the `:id` in the route
   Shoe.findById(req.params.id)
     .populate('owner', 'email')
     .then(handle404)
-    // if `findById` is succesful, respond with 200 and "question" JSON
-    .then(question => res.status(200).json({ question: question.toObject() }))
+    // if `findById` is succesful, respond with 200 and "shoe" JSON
+    .then(shoe => res.status(200).json({ shoe: shoe.toObject() }))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
 
 // SHOW
-// GET /questions/5a7db6c74d55bc51bdf39793
+// GET /shoes/
 router.get('/mysurvey/:id', requireToken, (req, res, next) => {
   // req.params.id will be set based on the `:id` in the route
   const userId = req.user._id
@@ -55,13 +55,13 @@ router.get('/mysurvey/:id', requireToken, (req, res, next) => {
 })
 
 // CREATE
-// POST /questions
+// POST /shoes
 router.post('/shoes', requireToken, (req, res, next) => {
-  // set owner of new question to be current user
+  // set owner of new shoe to be current user
   req.body.shoe.owner = req.user.id
 
   Shoe.create(req.body.shoe)
-    // respond to succesful `create` with status 201 and JSON of new "question"
+    // respond to succesful `create` with status 201 and JSON of new "shoe"
     .then(shoe => {
       res.status(201).json({ shoe: shoe.toObject() })
     })
@@ -72,7 +72,7 @@ router.post('/shoes', requireToken, (req, res, next) => {
 })
 
 // UPDATE
-// PATCH /questions/5a7db6c74d55bc51bdf39793
+// PATCH /shoes/5a7db6c74d55bc51bdf39793
 router.patch('/shoes/:id', requireToken, removeBlanks, (req, res, next) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
@@ -95,14 +95,14 @@ router.patch('/shoes/:id', requireToken, removeBlanks, (req, res, next) => {
 })
 
 // DESTROY
-// DELETE /questions/5a7db6c74d55bc51bdf39793
+// DELETE /shoes/5a7db6c74d55bc51bdf39793
 router.delete('/shoes/:id', requireToken, (req, res, next) => {
   Shoe.findById(req.params.id)
     .then(handle404)
     .then(shoe => {
-      // throw an error if current user doesn't own `question`
+      // throw an error if current user doesn't own `shoe`
       requireOwnership(req, shoe)
-      // delete the question ONLY IF the above didn't throw
+      // delete the shoe ONLY IF the above didn't throw
       shoe.deleteOne()
     })
     // send back 204 and no content if the deletion succeeded
